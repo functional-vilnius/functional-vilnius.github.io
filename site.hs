d58baf9375542b328f85a9163f403ac2f176a2e8
Ignas Vyšniauskas <baliulia@gmail.com>
2015-03-02 00:10:17 +0200

    Add logo, remove old announcement from index

--- site.hs
+++ site.hs
@@ -7,7 +7,7 @@ import           Hakyll
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
+    match "img/*" $ do
         route   idRoute
         compile copyFileCompiler
 
